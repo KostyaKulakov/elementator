0fde74c1b758456882bd2d950b0d8ce506c49225
Константин Кулаков <konstantin.kulakov98@gmail.com>
2019-04-02 04:23:13 +0300

    Added Elementor board and handler data view

---
+++ Elementator/Elementator/Presenter/ElementorBoardPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  ElementorBoardPresenter.swift
+//  Elementator
+//
+//  Created by Konstantin Kulakov on 02/04/2019.
+//  Copyright © 2019 Konstantin Kulakov. All rights reserved.
+//
+
+import Foundation
